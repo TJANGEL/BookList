7ffbfcf740f60308dbefda70b6bdea7fb7755790
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-12-25 10:38:18 -0500

    populated app.js

--- app.js
+++ app.js
@@ -34,14 +34,52 @@ class UI {
 
     row.innerHTML = `
         <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  //   clear input fields after submit
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
   }
 }
 
 // Store Class: Handles Storage
 
 // Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", e => {
+  //  Prevent submit
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  //   instantiate book
+  const book = new Book(title, author, isbn);
+
+  //   add book to UI
+  UI.addBookToList(book);
+
+  //   clear fields
+  UI.clearFields();
+});
 
 // Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
