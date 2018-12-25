43fd4d7817aa5db604dabb341e4f6c3edf1dbc03
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-12-25 10:52:25 -0500

    update to app.js

--- app.js
+++ app.js
@@ -47,6 +47,20 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    // remove after 3 sec
+    setTimeout(() => document.querySelector(".alert").remove(), 2000);
+  }
+
   //   clear input fields after submit
   static clearFields() {
     document.querySelector("#title").value = "";
@@ -56,6 +70,13 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+  static getBooks() {}
+
+  static addBook(book) {}
+
+  static removeBook(isbn) {}
+}
 
 // Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
@@ -69,17 +90,28 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  //   instantiate book
-  const book = new Book(title, author, isbn);
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    //   instantiate book
+    const book = new Book(title, author, isbn);
 
-  //   add book to UI
-  UI.addBookToList(book);
+    //   add book to UI
+    UI.addBookToList(book);
 
-  //   clear fields
-  UI.clearFields();
+    // show sucessful submit message
+    UI.showAlert("Book Added", "success");
+
+    //   clear fields
+    UI.clearFields();
+  }
 });
 
 // Event: Remove a Book
 document.querySelector("#book-list").addEventListener("click", e => {
   UI.deleteBook(e.target);
+
+  // show book removed message
+  UI.showAlert("Book Removed", "danger");
 });
